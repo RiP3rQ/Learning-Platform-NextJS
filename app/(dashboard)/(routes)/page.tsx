f01f0d642f4304a0e16952718d1b5a7ad7bb9d31
RiP3rQ <rafalpompa2000@gmail.com>
2023-09-19 14:09:31 +0200

    navbar

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <h1>
-      <UserButton afterSignOutUrl="/" />
-    </h1>
-  );
+  return <h1>Treść</h1>;
 }
