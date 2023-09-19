74c297c989ecb488c37afe16bd5926c3e3f6ea4a
RiP3rQ <rafalpompa2000@gmail.com>
2023-09-19 17:42:31 +0200

    !IMPORTANT! (seeding) Importing data to database using "node scripts/seed.ts"

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: "Computer Science" },
+        { name: "Music" },
+        { name: "Fitness" },
+        { name: "Photography" },
+        { name: "Accounting" },
+        { name: "Engineering" },
+        { name: "Filming" },
+      ],
+    });
+
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await db.$disconnect();
+  }
+}
+
+main();
