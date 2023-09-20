952432604661a10017c563be175940920e21694b
RiP3rQ <rafalpompa2000@gmail.com>
2023-09-20 15:24:30 +0200

    categories and search bar functionalities for browse page

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+import { db } from "@/lib/db";
+import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+
+import { Categories } from "./_components/categories";
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
+
+  console.log(courses);
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6 space-y-4">
+        <Categories items={categories} />
+      </div>
+    </>
+  );
 };
 
 export default SearchPage;
